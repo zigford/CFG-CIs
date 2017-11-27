06414c5ac4b59f0dcb26ecd3ed62564beb375b9e
Jesse Harris <jpharris@usc.edu.au>
2017-11-28 09:45:19 +1000

    Repair of MSI was forcing a reboot.

--- Scripts/Application-DellCommandMonitor-Compliance.ps1
+++ Scripts/Application-DellCommandMonitor-Compliance.ps1
@@ -1,18 +1,33 @@
 # Check if it's installed
+$Global:logFile = "$env:WinDir\AppLog\Application-DellCommandMonitor-Compliance.ps1.log"
+function logMsg {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(
+            ValueFromPipeline=$True
+        )]$Message,$logFile=$Global:logFile
+    )
+    "$(Get-Date): $Message" | Out-File -FilePath $logFile -Append
+}
+
 $ARPEntry = get-childitem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ | ?{(Get-ItemProperty -Path $_.PSPath -Name DisplayName -EA SilentlyContinue).DisplayName -match 'Dell Command | Monitor'}
 If (-Not $ARPEntry) {
     #  If the Add remove programs entry doesn't exist. The app state is compliant
     return $True
-}
-# After that, we know it's installed. Lets perform a WMI test to confirm the service is functional
-
-Try {
-    Get-WMIObject -NameSpace root\dcim\sysman -Class DCIM_BiosService -ErrorAction Stop | Out-Null
-    # If we have gotten here without an exception. The service is compliant.
-    $True
-} Catch {
-    # Assuming we have hit an exception, this means that we cannot attach to the service and the app needs to be repaired
-    # As this is a compliance script. We will simply return non compliant
-    $False
-    # Start-Process -FilePath msiexec.exe -ArgumentList "/fam $($ARPEntry.PSChildName) /qn" -Wait -PassThru
+    logMsg "Dell Command - Monitor does not have an ARP entry. Compliant"
+} else {
+    # After that, we know it's installed. Lets perform a WMI test to confirm the service is functional
+    logMsg "Attempting to query WMI to see if Dell Command is functional"
+    Try {
+        Get-WMIObject -NameSpace root\dcim\sysman -Class DCIM_BiosService -ErrorAction Stop | Out-Null
+        # If we have gotten here without an exception. The service is compliant.
+        logMsg "Succesfully queried the DCIM\Sysman DCIM_BiosService. Returning True"
+        return $True
+    } Catch {
+        # Assuming we have hit an exception, this means that we cannot attach to the service and the app needs to be repaired
+        # As this is a compliance script. We will simply return non compliant
+        logMsg "Failed to query the DCIM\Sysman DCIM_BiosService. Returning False"
+        return $False
+        # Start-Process -FilePath msiexec.exe -ArgumentList "/fam $($ARPEntry.PSChildName) /qn" -Wait -PassThru
+    }
 }
--- Scripts/Application-DellCommandMonitor-Remediate.ps1
+++ Scripts/Application-DellCommandMonitor-Remediate.ps1
@@ -1,10 +1,23 @@
+$Global:logFile = "$env:WinDir\AppLog\Application-DellCommandMonitor-Remediate.ps1.log"
+function logMsg {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(
+            ValueFromPipeline=$True
+        )]$Message,$logFile=$Global:logFile
+    )
+    "$(Get-Date): $Message" | Out-File -FilePath $logFile -Append
+}
 # Get ARP Entry
 $ARPEntry = get-childitem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ | ?{(Get-ItemProperty -Path $_.PSPath -Name DisplayName -EA SilentlyContinue).DisplayName -match 'Dell Command | Monitor'}
 If (-Not $ARPEntry) {
     #  If the Add remove programs entry doesn't exist. This kinda can't happen.
+    logMsg "Unable to find Dell Command | Monitor ARP Entry. Exiting with an error"
     Write-Error "Unable to find Dell Command | Monitor ARP Entry" -ErrorAction Stop
 }
 # After that, we know it's installed. Lets repair the MSI
 
-$Result = Start-Process -FilePath msiexec.exe -ArgumentList "/fam $($ARPEntry.PSChildName) /qn" -Wait -PassThru
-Write-Verbose "ExitCode: $($Resule.ExitCode)"
+logMsg "Starting repair process. Command:"
+logMsg "msiexec.exe /fam $($ARPEntry.PSChildName) /qn REBOOT=REALLYSUPPRESS"
+$Result = Start-Process -FilePath msiexec.exe -ArgumentList "/fam $($ARPEntry.PSChildName) /qn REBOOT=REALLYSUPPRESS" -Wait -PassThru
+logMsg "ExitCode: $($Result.ExitCode)"
