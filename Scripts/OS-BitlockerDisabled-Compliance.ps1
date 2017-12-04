c4e1648ee4aee6d63db7c092f0a113da0275edc5
Jesse Harris <jpharris@usc.edu.au>
2017-12-04 11:49:38 +1000

    Added logic to include drives that return an error when the drive is not encryptable. Thus preventing from returning compliant

--- Scripts/OS-BitlockerDisabled-Compliance.ps1
+++ Scripts/OS-BitlockerDisabled-Compliance.ps1
@@ -1,7 +1,14 @@
-$Bitlocker = Get-BitLockerVolume -MountPoint $env:SystemDrive
-#Meet the following rules, ProtectionStatus not = On, No KeyProtectors exist and there is no VolumeStatus
-If (($Bitlocker.ProtectionStatus.ToString() -ne 'On') -and (-Not ($Bitlocker.KeyProtector)) -and ($Bitlocker.VolumeStatus -eq 'FullyDecrypted')) {
-    $True
+
+$Bitlocker = Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue
+#Meet the following rules, ProtectionStatus not = On, No KeyProtectors exist and there is no VolumeStatus. Warning. This script may error if 
+If ($BitLocker) {
+    If (($Bitlocker.ProtectionStatus.ToString() -ne 'On') -and (-Not ($Bitlocker.KeyProtector)) -and ($Bitlocker.VolumeStatus -eq 'FullyDecrypted')) {
+        # return true here because protection is not on and volue is fullydecrypted
+        return $True
+    } else {
+        return $False
+    }
 } else {
-    $False
+    # return true here because the drive cannot be encrypted anyway
+    return $True
 }
